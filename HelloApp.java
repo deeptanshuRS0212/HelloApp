4c3b17074b00c3ee609f5a0807426b7ce6dd2cd3
deeptanshuRS0212 <deeptanshu282@gmail.com>
2026-05-07 23:23:44 +0530

    UC3: Display name argument or default World

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String userName = "World";
+
+        if (args.length > 0) {
+            userName = args[0];
+        }
+
+        System.out.println("Hello, " + userName + "!");
     }
 }
